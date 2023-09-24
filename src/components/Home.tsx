c21a396dbb07646476104fc9cc4fdfc0436e24a7
Sanjeet Yadav <87947328+dukendev@users.noreply.github.com>
2023-09-24 11:02:26 +0000

    updated home copy header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,8 @@ function Home() {
         <section>        
             <div class="w-full h-400  px-10 py-80 text-start bg-transparent z-10 text-white">
 
-                    <h1 class="text-6xl p-2">Hi, I am Sanjeet Yadav</h1>
-                    <h2 class=" text-3xl px-2 text-cyan-300">Software Development Engineer</h2>
+                    <h1 class="text-6xl p-2">Hey, I'm Sanjeet  ¯\\_(ツ)_/¯</h1>
+                    <h2 class=" text-3xl px-2 text-cyan-300">Software Engineer</h2>
                     <p class="px-2 mt-4">
                         I am professional     
                         <span class="inline-flex pt-2 type-profession text-cyan-300 text-2xl duration-150">
